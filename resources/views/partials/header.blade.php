9962895066b5670ef38462e9fffb122ce70715d8
MayaSelena773 <maya.bono721@gmail.com>
2022-08-25 23:45:41 +0200

    header begin

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,4 +1,31 @@
 <header>
-    <div>Header top</div>
-    <div>Header bottom</div>
+<!--Header Top-->
+    <div class="header-top">
+        <a href="#">dc power visa</a>
+        <a href="#">additional dc site</a>
+    </div>
+
+ <!--Header Bottom-->   
+    <div class="header-bottom">
+        <!--Logo-->
+        <div class="logo">
+            <img src="{{ asset('img/dc-logo.png') }}" alt="logo">
+        </div>
+
+        <!--Nav-->
+        <nav>
+            <ul class="flex">
+                <li><a href="#">Characters</a></li>
+                <li><a href="#">Comics</a></li>
+                <li><a href="#">Movies</a></li>
+                <li><a href="#">Tv</a></li>
+                <li><a href="#">Games</a></li>
+                <li><a href="#">Collectibles</a></li>
+                <li><a href="#">Videos</a></li>
+                <li><a href="#">Fans</a></li>
+                <li><a href="#">News</a></li>
+                <li><a href="#">Shop</a></li>
+            </ul>
+        </nav>
+    </div>
 </header>
