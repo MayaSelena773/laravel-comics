f8872eccedeec815356288780045999dcc45b52c
MayaSelena773 <maya.bono721@gmail.com>
2022-08-26 00:42:57 +0200

    header scss

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,12 +1,14 @@
 <header>
 <!--Header Top-->
     <div class="header-top">
+        <div class="container">
         <a href="#">dc power visa</a>
         <a href="#">additional dc site</a>
+        </div>
     </div>
 
  <!--Header Bottom-->   
-    <div class="header-bottom">
+    <div class="header-bottom container">
         <!--Logo-->
         <div class="logo">
             <img src="{{ asset('img/dc-logo.png') }}" alt="logo">
@@ -16,7 +18,7 @@
         <nav>
             <ul class="flex">
                 <li><a href="#">Characters</a></li>
-                <li><a href="#">Comics</a></li>
+                <li class="current"><a href="#">Comics</a></li>
                 <li><a href="#">Movies</a></li>
                 <li><a href="#">Tv</a></li>
                 <li><a href="#">Games</a></li>
@@ -25,6 +27,7 @@
                 <li><a href="#">Fans</a></li>
                 <li><a href="#">News</a></li>
                 <li><a href="#">Shop</a></li>
+                <li><a href="#">Search</a></li>
             </ul>
         </nav>
     </div>
